5639ee7036d20ef560be9699d97920aaf66dd5cb
Rafaisya Dwi Adrianto <147736360+Clafa30@users.noreply.github.com>
2023-11-05 13:37:36 +0700

    Update main.cpp

--- Praktikum4/main.cpp
+++ Praktikum4/main.cpp
@@ -38,15 +38,23 @@ int main(){
 		loopFunction();
 
 		cout << "\nDeret Bilangan Genap : ";
-		for(int genap=1;genap<=batas;genap++) {if (genap%2==0) cout<<genap<<" ";} 
+		for(int genap=1;genap<=batas;genap++) {
+			if (genap%2==0) 
+				cout<<genap<<" ";
+		} 
 		  
 		sum = 0;
-		  for (int genap=1; genap <= batas; genap++) {if (genap%2==0){sum += genap;}} 
-			cout << "\n\nIni adalah hasil Jumlah dari Deret Bilangan Genap : " << sum;
-		  	cout << "\nFaktor bilangan dari " << sum << " adalah 		  : ";
+		  for (int genap=1; genap <= batas; genap++) {
+			  if (genap%2==0){
+				  sum += genap;
+			  }
+		  } 
+		cout << "\n\nIni adalah hasil Jumlah dari Deret Bilangan Genap : " << sum;
+		cout << "\nFaktor bilangan dari " << sum << " adalah 		  : ";
     		for (int genap = sum; genap > 0; --genap){
             	if (sum % genap == 0){
-                	cout << genap << " ";}
+                	cout << genap << " ";
+		}
         		} cout << endl;
 
 		loopFunction();
@@ -66,21 +74,18 @@ int main(){
 		  for (int prima=1; prima <= batas; prima++) {
 			int bil=0;
 			for(int j=1;j<=prima;j++){
-				if(prima%j==0){
-				bil++;
-				}
-			} if (bil==2){ 
-				sum += prima;
-				}
-		  } cout << "\n\nIni adalah hasil Jumlah dari Deret Bilangan Prima : " << sum;
-		  cout << "\nFaktor bilangan dari " << sum << " adalah 		  : ";
+				if(prima%j==0){bil++;}
+			} if (bil==2){sum += prima;}
+		  } 
+		cout << "\n\nIni adalah hasil Jumlah dari Deret Bilangan Prima : " << sum;
+		cout << "\nFaktor bilangan dari " << sum << " adalah 		  : ";
     		for (int prima = sum; prima > 0; --prima){
-            if (sum % prima == 0){
-                cout << prima << " ";
-            }
+            		if (sum % prima == 0){
+                	cout << prima << " ";
+            	}
         } cout << endl;
 
 		loopFunction();
-	return 0;
+return 0;
 }
 	
