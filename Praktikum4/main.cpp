b1e90b1dfc5555bd7c461c9763ca2a28ff977cc0
Rafaisya Dwi Adrianto <147736360+Clafa30@users.noreply.github.com>
2023-11-06 09:05:04 +0700

    Update main.cpp

--- Praktikum4/main.cpp
+++ Praktikum4/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void loopFunction(){ 
+void pembatas(){ 
     for (int i = 1; i <= 150; i++) {
     cout << "-";
   }
@@ -9,83 +9,71 @@ void loopFunction(){
 
 int main(){
 	int batas, sum;
-
-	loopFunction();
-
+	
+	pembatas();
+	do {
 	cout << "\nMasukan Batas (Hanya Bisa Bilangan Positif) : ";
 	cin >> batas; //Input disini untuk memberikan batas pada jumlah deret yang mau ditampilkan.
-
-	loopFunction();
-
+	} while (batas <=0);
+	
+	pembatas();
 		cout << "\nDeret Bilangan Ganjil : ";
 		for(int ganjil=1; ganjil <= batas; ganjil++){
-		  if (ganjil%2!=0)
-		  cout << ganjil << " ";
-	      } 
-		  
-		sum = 0;
+		  if (ganjil%2!=0){
+		  	cout << ganjil << " ";
+	      		} 
+		}
 		  for (int ganjil=1; ganjil <= batas; ganjil++) {
 			if (ganjil%2!=0){
-			sum += ganjil;}
-		  } cout << "\n\nIni adalah hasil Jumlah dari Deret Bilangan Ganjil : " << sum;
-		  cout << "\nFaktor bilangan dari " << sum << " adalah 		   : ";
-    		for (int ganjil = sum; ganjil > 0; --ganjil){
-            if (sum % ganjil == 0){
-                cout << ganjil << " ";
-            }
-        } cout << endl;
-		  
-		loopFunction();
-
-		cout << "\nDeret Bilangan Genap : ";
-		for(int genap=1;genap<=batas;genap++) {
-			if (genap%2==0) 
-				cout<<genap<<" ";
-		} 
-		  
-		sum = 0;
-		  for (int genap=1; genap <= batas; genap++) {
-			  if (genap%2==0){
-				  sum += genap;
-			  }
-		  } 
-		cout << "\n\nIni adalah hasil Jumlah dari Deret Bilangan Genap : " << sum;
-		cout << "\nFaktor bilangan dari " << sum << " adalah 		  : ";
-    		for (int genap = sum; genap > 0; --genap){
-            	if (sum % genap == 0){
-                	cout << genap << " ";
-		}
-        		} cout << endl;
-
-		loopFunction();
-
-		cout << "\nDeret bilangan prima : ";
-		for(int prima=1;prima<=batas;prima++){
-		int bil=0;
-		for(int j=1;j<=prima;j++){
-		if(prima%j==0){
-			bil++;
+			sum += ganjil;
 			}
-		} if (bil==2)
-		cout << prima << " ";
-		}
-
-		sum = 0;
-		  for (int prima=1; prima <= batas; prima++) {
-			int bil=0;
-			for(int j=1;j<=prima;j++){
-				if(prima%j==0){bil++;}
-			} if (bil==2){sum += prima;}
 		  } 
+		cout << "\n\nIni adalah hasil Jumlah dari Deret Bilangan Ganjil : " << sum;
+		cout << "\nFaktor bilangan dari " << sum << " adalah 		   : ";
+    		for (int ganjil = sum; ganjil > 0; --ganjil){
+            		if (sum % ganjil == 0){
+                	cout << ganjil << " ";
+            		}
+        	} 
+        sum = 0; //Sum =0 disini untuk mereset kembali hasil dari penjumlahan deret diatasanya agar menjadi 0, agar tidak ikut ter jumlah di sum selanjutnya
+        cout << endl;
+		  
+	pembatas();
+		cout << "\nDeret Bilangan Genap : ";
+		for(int genap=1;genap<=batas;genap++) {if (genap%2==0) cout<<genap<<" ";} 
+		  
+		for (int genap=1; genap <= batas; genap++) {if (genap%2==0){sum += genap;}} 
+			cout << "\n\nIni adalah hasil Jumlah dari Deret Bilangan Genap : " << sum;
+		  	cout << "\nFaktor bilangan dari " << sum << " adalah 		  : ";
+    		for (int genap = sum; genap > 0; --genap){
+            		if (sum % genap == 0){
+                		cout << genap << " ";}
+        			} 
+        sum = 0;
+        cout << endl;
+
+	pembatas();
+        cout << "\nDeret bilangan prima : ";
+		for (int prima = 2; prima <= batas; ++prima){
+			int bilanganPrima = 1;
+			for (int j = 2; j * j <= prima; ++j){
+				if (prima % j == 0){
+				bilanganPrima = 0;
+				break;
+				}
+			}
+			if (bilanganPrima == 1){
+				cout << prima << " ";
+				sum += prima;
+				}
+			} 
 		cout << "\n\nIni adalah hasil Jumlah dari Deret Bilangan Prima : " << sum;
 		cout << "\nFaktor bilangan dari " << sum << " adalah 		  : ";
-    		for (int prima = sum; prima > 0; --prima){
-            		if (sum % prima == 0){
-                	cout << prima << " ";
-            	}
-        } cout << endl;
-
-		loopFunction();
+    			for (int prima = sum; prima > 0; --prima){
+            			if (sum % prima == 0){
+                		cout << prima << " ";
+            			}
+        	} cout << endl;
+	pembatas();
 return 0;
 }
-	
